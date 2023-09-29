17d170a2a3844b171c5fcbd03d2a63bab13a3107
Simone <simo.brazzo@gmail.com>
2023-09-29 13:28:10 +0200

    feat:location permission official return values

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'camera_screen.dart'; // Import the camera screen file
-import 'utils/geo_utilities.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+Future<LocationPermission> initPosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Check if location services are enabled
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return LocationPermission.unableToDetermine;
+  }
+
+  // Request location permission
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    return await Geolocator.requestPermission();
+  }
+
+  return LocationPermission.unableToDetermine;
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Initialize a variable to track whether location data has been loaded
+  late LocationPermission locationDataLoaded =
+      LocationPermission.unableToDetermine;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Call initPosition() asynchronously and wait for it to complete
+    initPosition().then((value) {
+      setState(() {
+        locationDataLoaded = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'cultural-arts.com app',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'cultural-arts.com'),
+      home: _buildHomeWidget(locationDataLoaded),
+    );
+  }
+}
+
+Widget _buildHomeWidget(LocationPermission locationDataLoaded) {
+  switch (locationDataLoaded) {
+    case LocationPermission.whileInUse:
+      return const MyHomePage(title: 'cultural-arts.com');
+    case LocationPermission.always:
+      return const MyHomePage(title: 'cultural-arts.com');
+    case LocationPermission.denied:
+      return const LoadingScreen();
+    case LocationPermission.deniedForever:
+      return const LoadingScreen();
+    default:
+      return const LoadingScreen();
+  }
+}
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(), // Show a loading indicator
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -63,40 +105,22 @@ class _MyHomePageState extends State<MyHomePage> {
   void _enableCameraPreview() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      // changed in this State, which causes it to rerun the build method
       enableTakePicture = true;
     });
-  }
-
-  Future<void> initPosition() async {
-    Position currentPosition = await getPosition();
   }
 
   @override
   void initState() {
     super.initState();
-    initPosition();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // This method is rerun every time setState is called
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Container(
